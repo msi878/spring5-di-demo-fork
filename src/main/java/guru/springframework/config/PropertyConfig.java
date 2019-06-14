1c9c30d92d4ef83d299b533b696de1cba8c65622
msi878 <info@mfinco.it>
2019-06-14 11:09:47 +0200

    close 97

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,23 +1,22 @@
 package guru.springframework.config;
 
-import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 
 /**
  * Created by jt on 6/7/17.
  */
 @Configuration
-@PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -27,19 +26,32 @@ public class PropertyConfig {
 
     @Value("${guru.dburl}")
     String url;
+    
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
 
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
     }
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =new PropertySourcesPlaceholderConfigurer();
-        return  propertySourcesPlaceholderConfigurer;
+    public FakeJmsBroker fakeJmsBroker(){
+    	FakeJmsBroker jmsBroker = new FakeJmsBroker();
+    	jmsBroker.setUsername(jmsUsername);
+    	jmsBroker.setPassword(jmsPassword);
+    	jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
     }
+    
 }
